7cf3a3a31c02eb1799867b89a576646a34b69c5e
Anirudh Kishore <anirudhkishore@Anirudhs-MBP.cable.rcn.com>
2021-02-07 15:04:17 -0500

    Created user service template

---
+++ wbdv-sp21-01-anirudh-server-java/src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,139 @@
+(function () {
+    var $usernameFld, $passwordFld;
+    var $firstNameFld, $lastNameFld, $roleFld;
+    var $removeBtn, $editBtn, $createBtn, $updateBtn;
+    var $userRowTemplate, $tbody;
+    var userService = new AdminUserServiceClient();
+    var users = [];
+    var selectedUser = null;
+
+    function createUser(user) {
+        userService.createUser(user)
+            .then(function (actualUser) {
+                users.push(actualUser)
+                renderUsers(users)
+            })
+    }
+
+    function deleteUser(event) {
+        //console.log(event.target)
+        var deleteBtn = $(event.target)
+        var theIndex = deleteBtn.attr("id")
+        var theId = users[theIndex]._id
+        //console.log(theIndex)
+
+        userService.deleteUser(theId)
+            .then(function (status) {
+                //console.log(status)
+                users.splice(theIndex, 1)
+                renderUsers(users)
+            })
+    }
+
+
+    function selectUser(event) {
+        var selectBtn = $(event.target)
+        var theId = selectBtn.attr("id")
+        selectedUser = users.find(user => user._id === theId)
+        $usernameFld.val(selectedUser.username)
+        $passwordFld.val(selectedUser.password)
+        $firstNameFld.val(selectedUser.firstName)
+        $lastNameFld.val(selectedUser.lastName)
+        $roleFld.val(selectedUser.role)
+    }
+
+    function updateUser() {
+        //console.log(selectedUser)
+        selectedUser.username = $usernameFld.val()
+        selectedUser.password = $passwordFld.val()
+        selectedUser.firstName = $firstNameFld.val()
+        selectedUser.lastName = $lastNameFld.val()
+        selectedUser.role = $roleFld.val()
+        //selectedUser._nuid = "001029510"
+        //selectedUser._domain = "users"
+        userService.updateUser(selectedUser._id, selectedUser)
+            .then(function (status) {
+                //console.log(status)
+                var index = users.findIndex(user => user._id === selectedUser._id)
+                users[index] = selectedUser
+                renderUsers(users)
+                $usernameFld.val("")
+                $passwordFld.val("")
+                $firstNameFld.val("")
+                $lastNameFld.val("")
+            })
+    }
+
+    function renderUsers(users) {
+        //console.log(users)
+        $tbody.empty()
+        for (var i = 0; i < users.length; i++) {
+            var user = users[i]
+            $tbody
+                .append(`
+                    <tr>
+                        <td>${user.username}</td>
+                        <td>${user.password}</td>
+                        <td>${user.firstName}</td>
+                        <td>${user.lastName}</td>
+                        <td>${user.role}</td>
+                        <td>
+                            <span class="pull-right">
+                                <i class="fa-2x fa fa-times wbdv-remove" id="${i}"></i>
+                                <i class="fa-2x fa fa-pencil wbdv-edit" id="${user._id}"></i>
+                            </span>
+                        </td>
+                    </tr>
+                `)
+        }
+        $(".wbdv-remove")
+            .click(deleteUser)
+        $(".wbdv-edit")
+            .click(selectUser)
+    }
+
+    function findAllUsers() {
+        userService.findAllUsers()
+            .then(function (actualUsers) {
+                users = actualUsers
+                renderUsers(users)
+            })
+    }
+
+    //function findUserById() { … } // optional - might not need this
+
+    function main() {
+        $usernameFld = $(".wbdv-username");
+        $passwordFld = $(".wbdv-password");
+        $firstNameFld = $(".wbdv-first-Name");
+        $lastNameFld = $(".wbdv-last-Name");
+        $roleFld = $(".wbdv-role");
+        $createBtn = $(".wbdv-create");
+        $removeBtn = $(".wbdv-remove");
+        $editBtn = $(".wbdv-edit");
+        $updateBtn = $(".wbdv-update");
+        $tbody = $(".wbdv-tbody");
+        //$userRowTemplate = $(".wbdv-template");
+
+        findAllUsers()
+
+        $createBtn.click(() => {
+            createUser({
+                username: $usernameFld.val(),
+                password: $passwordFld.val(),
+                firstName: $firstNameFld.val(),
+                lastName: $lastNameFld.val(),
+                role: $roleFld.val()
+            })
+            $usernameFld.val("")
+            $passwordFld.val("")
+            $firstNameFld.val("")
+            $lastNameFld.val("")
+        })
+
+        $removeBtn.click(deleteUser)
+        $editBtn.click(selectUser)
+        $updateBtn.click(updateUser)
+    }
+    $(main);
+})();
